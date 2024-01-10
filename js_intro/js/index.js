22a79c973657bb7692458aa894922d1dc217da92
Path1an <odintobiassen@gmail.com>
2024-01-10 09:18:51 +0100

    JS intro

---
+++ js_intro/js/index.js
@@ -0,0 +1,31 @@
+console.log('Hello World');
+//Oppgave A
+let firstName = 'Odin';
+let age = '16 years old';
+console.log (firstName, age);
+// Oppgave B: Den andre komandoen får å lage variabler er "var". Du skriver nøyaktig det samme bare bytter ut "let" med "var".
+
+// Oppgave C
+var erEnElev = true
+// Oppave D
+if(!erEnElev) {
+    console.log ( "Du er ikke en elev!");
+}else{
+    console.log ("Du er en elev!");
+}
+//Dette gjør at det står "Du er en elev!" I 'Console'. Dette er fordi jeg har satt denne variabelen til true.Hvis jeg skifyter den til false vill det stå "Du er ikke en elev!".
+
+//Del 2
+
+//Oppgave A
+console.log(2+2);
+//Oppgave B
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+console.log(11-(getRandomInt(11)));
+//oppgave C
+console.log(11*11);
+
+
+
