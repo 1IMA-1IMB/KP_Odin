1d16616c9171c59e89b67224b904664f4ef1f135
Path1an <odintobiassen@gmail.com>
2024-01-10 11:31:03 +0100

    ferdig_js-intro

--- js_intro/js/index.js
+++ js_intro/js/index.js
@@ -1,21 +1,21 @@
 console.log('Hello World');
 //Oppgave A
-let firstName = 'Odin';
-let age = '16 years old';
-console.log (firstName, age);
+let firstName = 'My name is Odin.';
+let age = 'I am 16 years old.';
+console.log ( firstName, age);
 // Oppgave B: Den andre komandoen får å lage variabler er "var". Du skriver nøyaktig det samme bare bytter ut "let" med "var".
 
 // Oppgave C
-var erEnElev = true
+let erEnElev = true
 // Oppave D
-if(!erEnElev) {
+if (!(erEnElev)) {
     console.log ( "Du er ikke en elev!");
 }else{
     console.log ("Du er en elev!");
 }
 //Dette gjør at det står "Du er en elev!" I 'Console'. Dette er fordi jeg har satt denne variabelen til true.Hvis jeg skifyter den til false vill det stå "Du er ikke en elev!".
 
-//Del 2
+//Del 2:
 
 //Oppgave A
 console.log(2+2);
@@ -25,7 +25,37 @@ function getRandomInt(max) {
   }
 console.log(11-(getRandomInt(11)));
 //oppgave C
-console.log(11*11);
+function myFunction(){
+    console.log(42*24);
+}
+myFunction();
+
+//Del 3:
+class Person  {
+    constructor(firstName,age){
+        this.firstName = firstName;
+        this.age = age;
+    }   
+    ferdigUtdannet(){
+        erEnElev = false;
+    }
+    bliElev(){
+        erEnElev = true;
+    }
+        
+  }
+  
+let Odin = new Person(' Jeg heter Odin','Jeg er 16 år');
+console.log(Odin.firstName)
+console.log(Odin.age);
+
+Odin.bliElev();
+if (erEnElev) {
+    console.log ( "Jeg er en elev!");
+}else{
+    console.log ("Jeg er ikke en elev!");
+}
+
 
 
 
