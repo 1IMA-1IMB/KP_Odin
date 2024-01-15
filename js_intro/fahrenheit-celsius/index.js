f02cd29032b8a89d6f157cb01a37c52f1d4a7473
Path1an <odintobiassen@gmail.com>
2024-01-15 09:47:03 +0100

    temperature converter

---
+++ js_intro/fahrenheit-celsius/index.js
@@ -0,0 +1,9 @@
+function fahrenheitConverter(valNum) {
+    valNum = parseFloat(valNum);
+    document.getElementById("outputCelsius").innerHTML = (valNum-32) / 1.8;
+    
+  }
+  function celsiusConverter(valNum) {
+    valNum = parseFloat(valNum);
+    document.getElementById("outputFahrenheit").innerHTML=(valNum*1.8)+32;
+  }
