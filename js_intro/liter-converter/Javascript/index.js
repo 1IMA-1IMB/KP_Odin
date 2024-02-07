0acd0c905bb31bb19ed697ebdb2b7dff74ab9831
Path1an <odintobiassen@gmail.com>
2024-02-07 10:48:40 +0100

    ferdig liter converter oppgave 1

--- js_intro/liter-converter/Javascript/index.js
+++ js_intro/liter-converter/Javascript/index.js
@@ -1,26 +1,43 @@
+
 function gallonConverter(valNum) {
     valNum = parseFloat(valNum);
     document.getElementById("outputLiter").innerHTML = (valNum*3.78541);
 }
+
 function literGallonConverter(valNum) {
     valNum = parseFloat(valNum);
     document.getElementById("outputGallon").innerHTML = (valNum/3.78541);
 }
 
+
 function ouncesConverter(valNum) {
     valNum = parseFloat(valNum);
     document.getElementById("outputLiter1").innerHTML = (valNum*0.0295735);
 }
+
 function literOuncesConverter(valNum) {
     valNum = parseFloat(valNum);
     document.getElementById("outputOunces").innerHTML = (valNum/0.0295735);
 }
 
+
 function cupsConverter(valNum) {
     valNum = parseFloat(valNum);
     document.getElementById("outputLiter2").innerHTML = (valNum*0.236588)
 }
+
 function literCupsConverter(valNum) {
     valNum = parseFloat(valNum);
     document.getElementById("outputCups").innerHTML = (valNum/0.236588);
+}
+
+
+function speiseskjeerConverter(valNum) {
+valNum = parseFloat(valNum);
+document.getElementById("outputLiter3").innerHTML = (valNum*0.0147868)
+}
+
+function literSpiseskjeerConverter(valNum) {
+    valNum = parseFloat(valNum);
+    document.getElementById("outputSpiseskjeer").innerHTML = (valNum/0.0147868)
 }
